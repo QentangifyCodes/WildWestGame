46785b42e157d4746c16bc9432388c5645ca8cc4
Kaysav Shaw <105513707+QentangifyCodes@users.noreply.github.com>
2022-12-10 17:19:48 -0500

    Added slopemovement
    Fixed a bug, added slope movement and changed terrain

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 [RequireComponent(typeof(Rigidbody))]
 [RequireComponent(typeof(CapsuleCollider))]
@@ -16,7 +17,7 @@ public class PlayerController : MonoBehaviour
     public float speed = 80;
     public float sprintingSpeed = 120;
     public float crouchSpeed = 50;
-    bool onSlope;
+    float height = 0;
 
     [Header("Limiters")]
     public float maxWalkSpeed = 100;
@@ -26,14 +27,18 @@ public class PlayerController : MonoBehaviour
     public float JumpPower = 300f;
     public float fallMultiplier = 2;
     bool isGrounded;
-    
+
+    [Header("Slope Handeling")]
+    public float maxSlopeAngle = 40;
+    RaycastHit slopeHit;
+
     // Movement States
     MovementStates state;
     enum MovementStates 
     {
         Walking,
         Sprinting,
-        Crouching
+        Crouching,
     }
 
     [Header("Coyote Time")]
@@ -44,6 +49,7 @@ public class PlayerController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         col = GetComponent<CapsuleCollider>();
+        height = col.height / 2;
     }
 
     private void Update()
@@ -57,38 +63,8 @@ public class PlayerController : MonoBehaviour
         HandleMovement();
     }
 
-    private void HandleJumping()
-    {
-        // Actual Check
-        isGrounded = Physics.Raycast(transform.position, Vector3.down, col.height/2+0.3f, LayerMask.GetMask("Ground")); // Ground Check
 
-        // Coyote Time Implenmentation and sticking to ground
-        if (isGrounded)
-        {
-            coyoteTimer = CoyoteTime;
-            rb.velocity = new Vector3(rb.velocity.x, -4.5f, rb.velocity.y);
-        }
-        else
-        {
-            if (coyoteTimer > -1)
-            {
-                coyoteTimer -= Time.deltaTime;
-            }
-        }
-
-        // Applying Force
-        if (Input.GetButtonDown("Jump") && coyoteTimer > 0)
-        {
-            rb.AddForce(transform.up * JumpPower);
-            coyoteTimer = 0;
-        }
-
-        // Fall Multiplier
-        if (rb.velocity.y < 0)
-        {
-            rb.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
-        }
-    }
+    // Handles movement states
     void HandleStates() 
     {
         // Changes state. Priority is sprinting
@@ -105,8 +81,12 @@ public class PlayerController : MonoBehaviour
             state = MovementStates.Walking;
         }
     }
+    
+    // Applies Movement
     void HandleMovement()
     {
+        rb.useGravity = !checkSlope();
+
         // Applying Force
         Vector3 movement = transform.forward * Input.GetAxisRaw("Vertical") + transform.right * Input.GetAxisRaw("Horizontal");
 
@@ -123,7 +103,19 @@ public class PlayerController : MonoBehaviour
         {
             curSpeed = crouchSpeed;
         }
-        rb.AddForce(curSpeed*movement.normalized, ForceMode.Acceleration);
+
+        if (checkSlope())
+        {
+            if (rb.velocity.y > 0) 
+            {
+                rb.AddForce(-0.01f * Vector3.down);
+            }
+            rb.AddForce(GetSlopeDirection(movement) * curSpeed, ForceMode.Force);
+        }
+        else 
+        {
+            rb.AddForce(curSpeed * movement.normalized, ForceMode.Acceleration);
+        }
 
         // Limiting Speed
         Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.y);
@@ -132,6 +124,57 @@ public class PlayerController : MonoBehaviour
             Vector3 maxVel = flatVel.normalized * maxSpeed;
             rb.velocity = new Vector3(maxVel.x, rb.velocity.y, maxVel.z);
         }
+    }
+
+    // Applies Jumping
+    private void HandleJumping()
+    {
+        // Actual Check
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, height + 0.1f, LayerMask.GetMask("Ground")); // Ground Check
+
+        // Coyote Time Implenmentation and sticking to ground
+        if (isGrounded)
+        {
+            coyoteTimer = CoyoteTime;
+        }
+        else
+        {
+            if (coyoteTimer > -1)
+            {
+                coyoteTimer -= Time.deltaTime;
+            }
+        }
+
+        // Applying Force
+        if (Input.GetButtonDown("Jump") && coyoteTimer > 0)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+            rb.AddForce(transform.up * JumpPower);
+            coyoteTimer = 0;
+        }
+
+        // Fall Multiplier
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+    }
+
+    // Checks if on slope
+    bool checkSlope()
+    {
+        if (Physics.Raycast(transform.position, Vector3.down, out slopeHit, height + 0.3f)) 
+        {
+            float angle = Vector3.Angle(Vector3.up, slopeHit.normal);
+            return angle < maxSlopeAngle && angle != 0;
+        }
+        return false;
+    }
+
+    // Edits direction if on slope
+    private Vector3 GetSlopeDirection(Vector3 direction) 
+    {
+        return Vector3.ProjectOnPlane(direction, slopeHit.normal);
     }
 
 }
